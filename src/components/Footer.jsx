189289dde5407243ab0cdc034d712dddebf8a20b
didjeram <alex_acurio@hotmail.com>
2025-03-07 04:15:44 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,7 @@ const Footer = () => {
     <footer className="footer bg-dark text-light py-3 mt-5">
       <div className="container text-center">
         <p className="footer__text mb-0">
-          &copy; {new Date().getFullYear()} Biblioteca Online. Todos los derechos reservados.
-        </p>
-        <p className="footer__links">
-          <a href="/about" className="text-light mx-2">Acerca de</a>
+          &copy; {new Date().getFullYear()} Biblioteca Online. Todos los derechos reservados. AAL
         </p>
       </div>
     </footer>
